be3c31298684db5f5475bf90f9939ed5225a5844
bearstand <richard.xiong.li@gmail.com>
2015-01-26 17:06:45 +0800

    add menu to the main activity.

--- src/com/xiong/richard/greyparrot/RecorderMainActivity.java
+++ src/com/xiong/richard/greyparrot/RecorderMainActivity.java
@@ -1,5 +1,6 @@
 package com.xiong.richard.greyparrot;
 
+import android.support.v7.app.ActionBarActivity;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,11 +12,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class RecorderMainActivity extends Activity {
+public class RecorderMainActivity extends ActionBarActivity {
 
 	private static final String LOG_TAG = "RecorderMainActivity";
 	private Button playButton = null;
@@ -228,4 +231,20 @@ public class RecorderMainActivity extends Activity {
 		lastSlash=fullPath.lastIndexOf('/');
 		return fullPath.substring(lastSlash+1);
 	}
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.config) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
